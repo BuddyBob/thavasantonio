d8d7a73ba85ead1e462a7a995b7ad1c1428746c0
Thavas Antonio <airstalk3r@gmail.com>
2022-06-04 22:20:04 -0700

    added box shadow

--- src/Home.js
+++ src/Home.js
@@ -11,15 +11,15 @@ export default function Home() {
             {/* banner */}
             <div className="banner-container">
                 <div className="inner-banner-container">
-                    <h1>Thavas Antonio's Blog</h1>
+                    <h1>Thavas Antonio's Posts</h1>
                 </div>
             </div>
             {/* post cards */}
             <div className="posts">
                 <div className="card-container">
-                    <Card date='April 28 2022' link="virtual-machine" title="Virtual Machine" description="A virtual machine is operating system inside your computer. This article will teach you how to create one."/>
-                    <Card date="June 1 2022" link="list-comprehension-python" title="List Comprehension in Python" description="List Comprehension is a one line syntactic construct to generate a list "/>
-                    <Card date="May 23 2022" link="vscode-extensions" title="Mi Favorito VS Code Extensiones " description="Visual Studio Code is a very powerful code editor. Here are my favorite extensions I use to improve productivity."/>
+                    <Card className="card" date='April 28 2022' link="virtual-machine" title="Virtual Machine" description="A virtual machine is operating system inside your computer. This article will teach you how to create one."/>
+                    <Card className="card" date="June 1 2022" link="list-comprehension-python" title="List Comprehension in Python" description="List Comprehension is a one line syntactic construct to generate a list "/>
+                    <Card className="card" date="May 23 2022" link="vscode-extensions" title="Mi Favorito VS Code Extensiones " description="Visual Studio Code is a very powerful code editor. Here are my favorite extensions I use to improve productivity."/>
                 </div>
             </div>
 
